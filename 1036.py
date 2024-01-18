513d98ae95d6285c0c9e699681afa0107838997b
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-01-18 14:19:16 -0300

    exercise baskhara

--- 1036.py
+++ 1036.py
@@ -10,10 +10,9 @@
 # Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, 
 # com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
 
+#a, b, c = map(float, input().split())
 
 import math
-from flask.scaffold import F
-
 
 a = float(input())
 b = float(input())
@@ -21,23 +20,21 @@ c = float(input())
 
 
 def bhaskara(a, b, c):
+    
     delta = (b**2) - (4 * (a * c))
-    if a == 0:
-        print("Impossivel Calcular")
+    
+    if (a == 0) or (delta < 0):
+        return None
     else:
         x1 = (- b + math.sqrt(delta))/(2 * a)
         x2 = (- b - math.sqrt(delta))/(2 * a)
-        if (x1<=0) or (x2<=0) or (a==0):
-            print("Impossivel Calcular")
-        else:
-            return x1, x2
-
-    if (x1<=0) or (x2<=0) or (a==0):
-        print("Impossivel Calcular")
-    else:
         return x1, x2
-    
+        
+resultado = bhaskara(a,b,c)
 
-x1, x2 = bhaskara(a, b, c)
-print(f"R1 = {x1:.5f}")
-print(f"R2 = {x2:.5f}")
+if resultado is None:
+    print("Impossivel calcular")
+else:
+    x1, x2 = resultado
+    print(f"R1 = {x1:.5f}")
+    print(f"R2 = {x2:.5f}")
