20a723a24be99d85eaaca9480e09d8cc0b0a021b
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-01-23 13:10:46 -0300

    multiple

---
+++ 1044.py
@@ -0,0 +1,18 @@
+#Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+#Entrada
+#A entrada contém valores inteiros.
+
+#Saída
+#A saída deve conter uma das mensagens conforme descrito acima.
+#a, b = map(int,input().split())
+a = int(input())
+b = int(input())
+
+def calc_multiplos(a, b):
+    if (b%a == 0)or (a%b==0):  
+        print("Sao Multiplos")
+    else:
+       print("Nao Sao Multiplos")
+       
+calc_multiplos(a,b)
