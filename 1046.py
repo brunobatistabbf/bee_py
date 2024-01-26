5e34004db31c9b92799a41bce28aae8f7f2dd11a
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-01-26 13:25:55 -0300

    Game Time Calc

--- 1046.py
+++ 1046.py
@@ -12,9 +12,16 @@
 horasIncial = int(input())
 horasFinal = int(input())
 
-def horas_jogando(horaInicial, horaFinal):
+def horas_jogando(horasInicial, horasFinal):
     if (horasIncial == 0) and (horasFinal == 0):
         print("O JOGO DUROU 24 HORA(S)")
-    pass 
+    else:
+        if horasInicial > horasFinal:
+            aux = 24 - horasInicial
+            horasTotal = aux + horasFinal
+            print(f"O JOGO DUROU %d HORA(S)"%horasTotal)
+        elif horasFinal > horasIncial:
+            horasTotal = horasFinal - horasIncial
+            print(f"O JOGO DUROU %d HORA(S)"%horasTotal)
 
 horas_jogando(horasIncial, horasFinal)
