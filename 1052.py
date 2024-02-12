0dd50fa7234b21e623441993023bc23afd41d0c2
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-02-12 14:12:45 -0300

    execise mouths

---
+++ 1052.py
@@ -0,0 +1,31 @@
+mouth = int(input())
+
+def definir_mes(mouth):
+    match mouth:
+        case 1:
+            print("January")
+        case 2:
+            print("February")
+        case 3:
+            print("March")
+        case 4:
+            print("April")
+        case 5:
+            print("May")
+        case 6:
+            print("June")
+        case 7:
+            print("July")
+        case 8:
+            print("August")
+        case 9:
+            print("September")
+        case 10:
+            print("Octuber")
+        case 11:
+            print("November")
+        case 12:
+            print("December")
+        
+
+definir_mes(mouth)
